793c233632b34a5bb0798be726e0770b3e46a1de
Raphael Kipsang <79073634+raphsang@users.noreply.github.com>
2024-10-16 13:15:39 +0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -14,6 +14,9 @@ from datetime import timedelta
 import os
 from pathlib import Path
 
+# Get the port from Render's environment variable
+PORT = os.getenv('PORT', 8000)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
